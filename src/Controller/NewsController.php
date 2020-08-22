4b8070d843e3f877dce44da79597a8bf8a67bde1
Oksana Pampushchenko <oksana2lucky@gmail.com>
2020-08-22 20:01:21 +0300

    Done news list

---
+++ src/Controller/NewsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\News;
+use App\Entity\Category;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class NewsController extends AbstractController
+{
+    /**
+     * @Route("/news", name="news")
+     */
+    public function index()
+    {
+        $news = $categories = $this->getDoctrine()->getRepository(News::class)
+            ->findBy(
+                [],
+                ['postedAt' => 'DESC'],
+                10
+            );
+
+        $categories = $this->getDoctrine()->getRepository(Category::class)
+            ->findAll();
+
+        return $this->render('news/index.html.twig', [
+            'categories' => $categories,
+            'news'  => $news
+        ]);
+    }
+}
+
+
